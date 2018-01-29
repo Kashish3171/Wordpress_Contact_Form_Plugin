50b5a11535a6ce8eb8199a1b340fdbe734e29763
Kashish Goyal <admin@kashishgoyal.in>
2018-01-29 22:41:55 +0530

    Add files via upload

--- Contact_Form_Plugin.php
+++ Contact_Form_Plugin.php
@@ -12,114 +12,96 @@ Author URI: http://www.kashishgoyal.in
 License: GPLv2 or later
 Text Domain: contact_form_plugin
 */
+
 function contactForm() {
 	//setting form action to current page
 	// esc_url escapes html characters that may brake the page
-	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
+	echo '<form action="'.esc_url($_SERVER['PHP_SELF']).'" method="post">';
 	/* Name Field in contact form */
 	echo '<p>';
 	echo 'Your Name<br/>';
-	echo '<input type="text" name="name" pattern="[a-zA-Z0-9 ]+" value="';
-	if(isset($_POST["name"])) 
-		{
-			echo esc_attr($_POST["name"]);
-		}
-	else
-		{
-			echo '';
-		}
-	echo '" size="40"  required/>';
+	echo '<input type="text" name="name" pattern="[a-zA-Z0-9 ]+" value="" size="40"  required/>';
 	echo '</p>';
 	/* Email Field in contact form */
 	echo '<p>';
 	echo 'Your Email<br/>';
-	echo '<input type="email" name="email" value="';
-	if(isset($_POST["email"]))
-		{
-			echo esc_attr($_POST["email"]);
-		}
-	else
-		{
-			echo '';
-		}
-	echo '" size="40"  required/>';
+	echo '<input type="email" name="email" value="" size="40"  required/>';
 	echo '</p>';
-	/*Subject Field in Contact Form */
-
-	echo '<p>';
-	echo 'Subject<br/>';
-	echo '<input type="text" name="subject" pattern="[a-zA-Z ]+" value="';
-	if(isset($_POST["subject"]))
-		{
-			 echo esc_attr($_POST["subject"]);
-		}
-	else
-		{
-			echo '';
-		}
-	echo '" size="40"  required/>';
-	echo '</p>';
-	/*Message Field in Contact Form */
-	echo '<p>';
-	echo 'Your Message<br/>';
-	echo '<textarea rows="12" cols="40" name="message" required>';
-	if(isset($_POST["message"]))
-		{
-			echo esc_attr($_POST["message"]);
-		}
-	else
-		{
-			echo '';
-		}
-	echo '</textarea>';
-	echo '</p>';
-	/*Submit Button */
+	/*Submit Button*/
 	echo '<p>';
 	echo 'All the above mentioned fields are neccessary<br/>';
 	echo '<input type="submit" name="form_submit" value="Send"></p>';
 	echo '</form>';
 }
+//if the user has submitted the form
+if(isset($_POST['form_submit']))
+{  
+	global $wpdb;
+	
 
-/* In case we want to mail the contact form */
-function sendmail() {
+	$table = $wpdb->prefix."user_details";
+	$name=filter_var($_POST['name'],FILTER_SANITIZE_STRING);
+	$email=filter_var($_POST['email'],FILTER_SANITIZE_STRING);
+	if(!filter_var($email,FILTER_VALIDATE_EMAIL)===false)
+	{
+	
+		$wpdb->insert( 
+		$table, 
+		array( 
+				'name' => $name,
+				'email' =>$email
 
-	// if the submit button is clicked, send the email
-	if ( isset( $_POST['form_submit'] ) ) {
-
-		// sanitize form values. 
-		// validating email value.
-		$name    = sanitize_text_field( $_POST["name"] );
-		$email   = sanitize_email( $_POST["email"] );
-		if(filter_var($email, FILTER_VALIDATE_EMAIL)===false)
-			exit('Invalid Email address');
-		$subject = sanitize_text_field( $_POST["subject"] );
-		$message = esc_textarea($_POST["message"] );
-
+			 )
+			);
 		
-		$to ='kashishgoyal3171@gmail.com';
-
-		$headers = "From: $name <$email>" . "\r\n";
-
-		// to check if mail sending was successful or not
-		if ( wp_mail( $to, $subject, $message, $headers ) ) {
-			echo '<div>';
-			echo '<p>Thanks for contacting me.</p>';
-			echo '</div>';
-		} else {
-			echo 'Mail sent failed';
-		}
 	}
+	header('Location: ' . $_SERVER["HTTP_REFERER"]);
+	exit;
+}
+// creates user_details in database if not exists
+function tablecheck()
+{  
+	global $wpdb;
+	$table = $wpdb->prefix . "user_details"; 
+	$charset_collate = $wpdb->get_charset_collate();
+	$sql = "CREATE TABLE IF NOT EXISTS $table (
+	`id` mediumint(9) NOT NULL AUTO_INCREMENT,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	UNIQUE (`id`)
+	) $charset_collate;";
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+}
+//fetches data from database
+function fetchdata()
+{  	global  $wpdb;
+	$table_name=$wpdb->prefix . "user_details";
+
+	$results = $wpdb->get_results( "SELECT * FROM $table_name");
+
+	echo "<table style='border:2px solid black;padding:25px;width:100%;background-color: #0393f414;'>";
+	foreach($results as $row)
+		{	
+				echo "<tr style='border:2px solid black;padding:25px;'>";
+				echo "<td style='border:2px solid black;padding:25px;'>".$row->name."</td>";
+
+				echo "<td style='border:2px solid black;padding:25px;'>".$row->email."</td>";
+
+				echo "</tr>";
+	
+		}
+	echo "</table>";
 }
 
-function lastfunction() {
-	//starting the output buffer
-	ob_start();
-	sendmail();
-	contactForm();
-	// cleaning the output buffer
-	return ob_get_clean();
+//this function is called firstly
+function lastFunction()
+{   tablecheck();
+	contactform();
+	fetchdata();
+
 }
 
-add_shortcode( 'contactKashish', 'lastfunction' );
+add_shortcode( 'contactKashish', 'lastFunction' );
 // to use this plugin activate it and then add [contactKashish]
 ?>
